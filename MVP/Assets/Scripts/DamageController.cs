cdb890c6d5ec2d0015b742a3c0a93264a1a151cb
LukeTrlin <luke.trlin@papamoacollege.school.nz>
2024-06-30 19:09:15 +1200

    Project Files

---
+++ MVP/Assets/Scripts/DamageController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+
+    public HealthManager healthManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            healthManager.TakeDamage(5);
+        }
+    }
+}
---
+++ MVP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    private float ActiveMoveSpeed;
+    public float DashSpeed;
+
+    private float DashLength = .5f, DashCooldown = 1f;
+
+    private float DashCounter;
+    private float DashCooldownCounter;
+    
+    private Vector2 moveDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ActiveMoveSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+    
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (DashCooldownCounter <= 0 && DashCounter <= 0)
+            {
+                ActiveMoveSpeed = DashSpeed;
+                DashCounter = DashLength;
+            }
+        }
+
+        if (DashCounter > 0)
+        {
+            DashCounter -= Time.deltaTime;
+
+            if (DashCounter <=0)
+            {
+                ActiveMoveSpeed = moveSpeed;
+                DashCounter = DashCooldown;
+            }
+        }
+
+        if (DashCooldownCounter > 0)
+        {
+            DashCooldownCounter -= Time.deltaTime;
+        }
+
+    }
+
+    void FixedUpdate () 
+    {
+        rb.velocity = moveDirection * moveSpeed;
+    }
+    
+}
