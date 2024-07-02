1ddb57d3ea9395f1b43947f4f9ab7e7feffc1c0e
LukeTrlin <luke.trlin@papamoacollege.school.nz>
2024-07-03 10:34:40 +1200

    Boundary Fix

--- MVP/Assets/Scripts/PlayerController.cs
+++ MVP/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
+    public float yRange = 7.5f;
+    public float xRange = 17.25f;
     private Rigidbody2D rb;
 
     public float moveSpeed;
@@ -28,6 +31,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (transform.position.x > xRange) {
+
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y > yRange) {
+
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+        if (transform.position.y < -yRange) {
+
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        if (transform.position.x < -xRange) {
+
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
         moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         
     
